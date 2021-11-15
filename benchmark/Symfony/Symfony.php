26f17ef4171753f3554adfcaf2474b7e8f9d78f1
Divine Niiquaye Ibok <divineibok@gmail.com>
2021-11-15 06:51:53 +0000

    Improved symfony's benchmark performance

--- benchmark/Symfony/Symfony.php
+++ benchmark/Symfony/Symfony.php
@@ -6,6 +6,7 @@ use BenchmarkRouting\Benchmark;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use PhpBench\Attributes as Bench;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Router;
 
 #[Bench\Groups(['symfony', 'raw'])]
@@ -14,6 +15,7 @@ final class Symfony extends Benchmark
     public function runRouting(string $route, string $method = 'GET'): array
     {
         $router = new Router(new PhpFileLoader(new FileLocator(__DIR__ . '/../../routes/')), 'symfony-routes.php');
+        $router->setOptions(['matcher_class' => UrlMatcher::class]);
         $router->getContext()->setMethod($method);
 
         return $router->match($route);
--- benchmark/Symfony/SymfonyInstance.php
+++ benchmark/Symfony/SymfonyInstance.php
@@ -6,6 +6,7 @@ use BenchmarkRouting\Benchmark;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use PhpBench\Attributes as Bench;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Router;
 
 #[Bench\Groups(['symfony', 'instance'])]
@@ -16,6 +17,7 @@ final class SymfonyInstance extends Benchmark
     public function __construct()
     {
         $this->router = new Router(new PhpFileLoader(new FileLocator(__DIR__ . '/../../routes/')), 'symfony-routes.php');
+        $this->router->setOptions(['matcher_class' => UrlMatcher::class]);
 
         // warmup.
         $this->benchAll();
